279066991b1c47bf85b3cd118c30d9e3bd958aaa
I'm Matheus <codelong@proton.me>
2025-05-17 15:55:36 -0300

    Fixing PATH

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -2,7 +2,7 @@ import pytest
 from monitor import check_discord_api, check_bot_discloud
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), './src')))
 
 def test_discord_api_status():
     status = check_discord_api()
