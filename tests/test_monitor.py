6fa0ae4a61bf707ac887b85afc2b2307f0dafa6a
I'm Matheus <codelong@proton.me>
2025-05-17 15:54:33 -0300

    From src.monitor to monitor

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,5 +1,5 @@
 import pytest
-from src.monitor import check_discord_api, check_bot_discloud
+from monitor import check_discord_api, check_bot_discloud
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
