62cb3ad9ee74ab95bfd9797f452496fa31bfa44e
I'm Matheus <codelong@proton.me>
2025-05-17 15:53:17 -0300

    Path into tests

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,5 +1,8 @@
 import pytest
 from src.monitor import check_discord_api, check_bot_discloud
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 def test_discord_api_status():
     status = check_discord_api()
