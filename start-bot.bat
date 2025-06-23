8b0f7b658047ec8fa1227cfa1df162bc0c2bdd16
I'm Matheus <codelong@proton.me>
2025-06-23 14:35:07 -0300

    Update start-bot.bat

--- start-bot.bat
+++ start-bot.bat
@@ -1,12 +1,13 @@
 @echo off
 setlocal
 
-set BOT_PATH=C:\Users\mmath\drive\Projects\MITPA\bot-beta
+for /f "tokens=1,2 delims==" %%a in (config.cfg) do (
+    if "%%a"=="BOT_PATH" set BOT_PATH=%%b
+)
 
 REM
 powershell -NoProfile -ExecutionPolicy Bypass -Command "Import-Module BurntToast; New-BurntToastNotification -Text 'MITPA Monitor', 'The beta bot is down and is being launched locally.'"
 
-REM
 cd /d "%BOT_PATH%"
 npm start
 
